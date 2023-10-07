31705b23a14417f90c7887cc3493d0063c642846
Luís Marques <euluismarques@gmail.com>
2023-10-07 23:55:10 +0100

    Remove base64 encoding

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -2,10 +2,10 @@ import { STORAGE_PREFIX } from '../config';
 
 const storage = {
   getToken: () => {
-    return atob(JSON.parse(window.localStorage.getItem(`${STORAGE_PREFIX}token`) as string));
+    return JSON.parse(window.localStorage.getItem(`${STORAGE_PREFIX}token`) as string);
   },
   setToken: (token: string) => {
-    window.localStorage.setItem(`${STORAGE_PREFIX}token`, JSON.stringify(btoa(token)));
+    window.localStorage.setItem(`${STORAGE_PREFIX}token`, JSON.stringify(token));
   },
   clearToken: () => {
     window.localStorage.removeItem(`${STORAGE_PREFIX}token`);
