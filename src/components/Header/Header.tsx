c16344149fa757250c9b5fbb974fc1d758567d83
Luís Marques <euluismarques@gmail.com>
2023-10-13 19:58:35 +0100

    Improve code separation in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import { NavItem } from '../NavItem/NavItem';
 
-import { useAuth, logout } from '../../features/auth';
+import { useAuth, logout, User } from '../../features/auth';
 import { useAppDispatch } from '../../hooks/store';
 import { APP_TITLE } from '../../config';
 
 export function Header() {
-  const dispatch = useAppDispatch();
-  const auth = useAuth();
-
-  const handleLogoutClick = () => {
-    dispatch(logout());
-  };
+  const { user } = useAuth();
 
   return (
     <nav className='navbar navbar-expand navbar-scroll bg-body-tertiary'>
@@ -20,21 +15,34 @@ export function Header() {
           {APP_TITLE}
         </NavLink>
 
-        <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
-          {auth.user ? (
-            <>
-              <NavItem text='Profile' href='/profile' />
-              <NavItem text='Settings' href='/settings' />
-              <NavItem type='button' text='Logout' onClick={handleLogoutClick} />
-            </>
-          ) : (
-            <>
-              <NavItem text='Login' href='/login' />
-              <NavItem text='Register' href='/register' />
-            </>
-          )}
-        </ul>
+        <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>{user ? <UserLinks user={user} /> : <GuestLinks />}</ul>
       </div>
     </nav>
+  );
+}
+
+function GuestLinks() {
+  return (
+    <>
+      <NavItem text='Login' href='/login' />
+      <NavItem text='Register' href='/register' />
+    </>
+  );
+}
+
+function UserLinks({ user: { firstName, email } }: { user: User }) {
+  const dispatch = useAppDispatch();
+  const name = firstName || email;
+
+  const handleLogoutClick = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <>
+      <NavItem text={`Hi, ${name}!`} href='/profile' />
+      <NavItem text='Settings' href='/settings' />
+      <NavItem type='button' text='Logout' onClick={handleLogoutClick} />
+    </>
   );
 }
