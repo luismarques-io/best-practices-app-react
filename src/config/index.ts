f7400a6fed48258deb48f949d5b5606f60f86228
Luís Marques <euluismarques@gmail.com>
2023-11-12 10:12:48 +0000

    Remove type from constants in config file that can be inferred

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const API_URL = process.env.REACT_APP_API_URL as string;
 export const STORAGE_PREFIX = 'general_purpose_app_';
 export const APP_TITLE = 'General Purpose App';
-export const JWT_SECRET = '123456' as string;
+export const JWT_SECRET = '123456';
