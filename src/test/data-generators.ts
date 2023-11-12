990305ccde5746ce1c0f16bebba93069d20b7bdd
Luís Marques <euluismarques@gmail.com>
2023-11-12 10:19:44 +0000

    Add data generators to test posts and comments

--- src/test/data-generators.ts
+++ src/test/data-generators.ts
@@ -13,3 +13,20 @@ export const userGenerator = (overrides?: Overrides) => ({
   image: faker.image.avatar(),
   ...overrides,
 });
+
+export const postGenerator = (overrides?: Overrides) => ({
+  id: faker.string.uuid(),
+  title: faker.lorem.sentence(),
+  body: faker.lorem.paragraphs({ min: 3, max: 10 }),
+  tags: faker.lorem.words(3).split(' '),
+  reactions: faker.number.int({ min: 0, max: 100 }),
+  createdAt: Date.now(),
+  ...overrides,
+});
+
+export const commentGenerator = (overrides?: Overrides) => ({
+  id: faker.string.uuid(),
+  body: faker.lorem.paragraph(),
+  createdAt: Date.now(),
+  ...overrides,
+});
