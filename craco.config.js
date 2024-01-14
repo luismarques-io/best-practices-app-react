53001e80c02e73f412a379fd808531840056a529
Luís Marques <euluismarques@gmail.com>
2024-01-14 13:22:44 +0000

    Added polifill to fix api mocking on the front-end

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,21 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+    plugins: [new NodePolyfillPlugin()],
+    configure: (webpackConfig) => {
+      const scopePluginIndex = webpackConfig.resolve.plugins.findIndex(
+        ({ constructor }) => constructor && constructor.name === 'ModuleScopePlugin'
+      );
+
+      webpackConfig.resolve.plugins.splice(scopePluginIndex, 1);
+      return webpackConfig;
     },
   },
 };
