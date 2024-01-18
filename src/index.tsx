c85db46665db08ee53a202f4fd599c996a17db2a
Luís Marques <euluismarques@gmail.com>
2024-01-18 19:18:11 +0000

    Add web-vitals to project

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 import { AppProvider } from './providers/AppProvider';
-
+import reportWebVitals from './reportWebVitals';
 import { initMocks } from './test/server';
 
 initMocks();
@@ -16,3 +16,8 @@ root.render(
     </AppProvider>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
